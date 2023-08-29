121a2a130d0638169c127c0a30cbd41b4327d304
decesarojunior <decesarojunior@yahoo.com.br>
2023-08-29 11:58:25 -0300

    implementacao da classe de Endereco, controlers e rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,9 @@
 import {Router} from 'express';
+import EnderecoController from './app/controllers/EnderecoController';
+
 const router = Router();
+
+//http://localhost:3000/endereco/list
+router.get('/endereco/list', EnderecoController.list);
+
 export default router;
